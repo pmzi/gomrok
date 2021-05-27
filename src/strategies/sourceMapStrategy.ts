6ade63a1fba63394edb4decc3467fbfb6c5fdb80
Pouya <pouya.mozafar@gmail.com>
2021-05-27 23:05:11 +0430

    First strategy added

--- src/strategies/sourceMapStrategy.ts
+++ src/strategies/sourceMapStrategy.ts
@@ -1,14 +1,37 @@
-import { IStrategy, StrategyRunnerOutputStatusEnum } from './shared/types';
+import fs from 'fs';
+import path from 'path';
+import { IStrategy, StrategyRunnerOutputStatusEnum, StrategyOutput } from './shared/types';
+
+const sourceMapFileNameReg = /\.map$/i;
+
+function checkForSourceMapLeak(src: string): StrategyOutput {
+  if (!fs.lstatSync(src).isDirectory()) return [];
+  const filesInsideSrc = fs.readdirSync(src);
+
+  const result: StrategyOutput = [];
+
+  for (const fileName of filesInsideSrc) {
+    const fullFilePath = path.resolve(src, fileName);
+
+    if (fs.lstatSync(fullFilePath).isDirectory()) {
+      result.push(...checkForSourceMapLeak(fullFilePath));
+    } else if (sourceMapFileNameReg.test(fileName)) {
+      result.push({
+        name: fullFilePath,
+        description: 'Sourcemap leak detected!',
+        status: StrategyRunnerOutputStatusEnum.Medium,
+      });
+    }
+  }
+
+  return result;
+}
 
 const toBeExported: IStrategy = {
-  name: '',
-  description: '',
+  name: 'sourceMapLeak',
+  description: 'This strategy checks whether any source maps are leaked or not.',
   async run({ src }) {
-    return [{
-      name: src,
-      description: 'salam',
-      status: StrategyRunnerOutputStatusEnum.Critical,
-    }];
+    return checkForSourceMapLeak(src);
   },
 };
 
